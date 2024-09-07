12f732b6098508f2e5790749fde4ad90cb80149f
Akshat <akshat9950@gmail.com>
2024-09-07 10:04:00 +0530

    Initial commit

---
+++ Assets/scripts/TapController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour {
+
+    public delegate void PlayerDelegate ();
+    public static event PlayerDelegate OnPlayerDied; // Event to notify GameManager when the player dies.
+    public static event PlayerDelegate OnPlayerScored; // Event to notify GameManager when the player scores.
+
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    public AudioSource tapAudio;
+    public AudioSource scoreAudio;
+    public AudioSource dieAudio;
+
+    Rigidbody2D rb;
+    Quaternion downrotation;
+    Quaternion forwardrotation;
+
+    GameManager game;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        downrotation = Quaternion.Euler(0, 0, -90);
+        forwardrotation = Quaternion.Euler(0, 0, 35);
+        game = GameManager.Instance;
+        rb.simulated = false;
+    }
+
+    void OnEnable(){
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+    void OnDisable(){
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameStarted(){
+        // Called when the countdown ends. Resets velocity and enables physics simulation.
+        rb.velocity = Vector3.zero;
+        rb.simulated = true;
+    }
+
+    void OnGameOverConfirmed(){
+        // Resets position and rotation when the replay button is clicked.
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
+    }
+
+    void Update(){
+        if (game.GameOver) {
+            rb.simulated = false;
+            return;
+        }
+
+        if(Input.GetMouseButtonDown(0)){
+            tapAudio.Play();
+            transform.rotation = forwardrotation;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downrotation, tiltSmooth * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+
+        if (col.gameObject.tag == "ScoreZone") {
+            OnPlayerScored(); // Event sent to GameManager when the player scores.
+            scoreAudio.Play();
+        }
+
+        if (col.gameObject.tag == "DeadZone") {
+            rb.simulated = false;
+            OnPlayerDied(); // Event sent to GameManager when the player dies.
+            dieAudio.Play();
+        }
+    }
+}
